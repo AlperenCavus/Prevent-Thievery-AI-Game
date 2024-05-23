d9e64738b6f4a3967aedd0e87b845770c2e3b283
AlperenCavus <116302486+AlperenCavus@users.noreply.github.com>
2024-05-23 15:42:10 +0300

    Update prevent-thievery.py

--- prevent-thievery.py
+++ prevent-thievery.py
@@ -4,3 +4,19 @@ import json
 import random
 import heapq
 import math
+
+# Initialize pygame
+pygame.init()
+
+# Define grid world constants
+GRID_SIZE = 15
+NUM_ACTIONS = 4
+ACTIONS = ['up', 'down', 'left', 'right']
+REWARDS = {'empty': -0.9, 'key': 50, 'chest': 100, 'hole': -50, 'spike': -30, 'guard': -100, 'finish': 120}
+GUARD_SYMBOL = 'G'
+CHEST_SYMBOL = 'C'
+KEY_SYMBOL = 'K'
+AGENT_SYMBOL = 'A'
+HOLE_SYMBOL = 'H'
+SPIKE_SYMBOL = 'S'
+EMPTY_SYMBOL = ' '
